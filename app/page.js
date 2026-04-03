602de85cb014cb2917ca3fa757b452f88ecad008
tdonelon <30850398+tdonelon@users.noreply.github.com>
2026-04-03 17:05:27 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,35 +1,43 @@
 import Link from 'next/link'
 
-export default function Page(){
+export default function Home(){
 
 return(
 
 <div style={{
 
-background:"#0B0F1A",
-color:"white",
-height:"100vh",
-padding:"40px"
+padding:"40px",
+textAlign:"center"
 
 }}>
 
-<h1>MN Elite Kern Hockey Hub</h1>
+<h1>
 
-<p>Elite Training for Elite Athletes</p>
+MN Elite Kern Hockey Hub
+
+</h1>
+
+<p>
+
+Elite Training for Elite Athletes
+
+</p>
 
 <Link href="/dashboard">
 
 <button style={{
 
-padding:"15px",
 background:"#00BFFF",
+padding:"20px",
 border:"none",
 color:"white",
-fontSize:"18px"
+fontSize:"20px",
+borderRadius:"8px",
+cursor:"pointer"
 
 }}>
 
-Enter App
+Enter Hockey Hub
 
 </button>
 
