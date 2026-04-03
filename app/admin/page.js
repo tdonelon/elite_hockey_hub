f909f7104e82144359e2f5ae4c35a61c0d4fa009
tdonelon <30850398+tdonelon@users.noreply.github.com>
2026-04-03 17:10:51 -0500

    Add Admin component with control buttons

---
+++ app/admin/page.js
@@ -0,0 +1,31 @@
+export default function Admin(){
+
+return(
+
+<div style={{padding:"20px"}}>
+
+<h1>Admin Control</h1>
+
+<button>
+
+Add Player
+
+</button>
+
+<button>
+
+Create Event
+
+</button>
+
+<button>
+
+Send Announcement
+
+</button>
+
+</div>
+
+)
+
+}
