ea0698ab09e949a699a1601562236b7d6cafecbe
tdonelon <30850398+tdonelon@users.noreply.github.com>
2026-04-03 17:10:09 -0500

    Add Homework component with stickhandling drill

---
+++ app/homework/page.js
@@ -0,0 +1,25 @@
+export default function Homework(){
+
+return(
+
+<div style={{padding:"20px"}}>
+
+<h1>Player Homework</h1>
+
+<p>
+
+Stickhandling drill video
+
+</p>
+
+<button>
+
+Mark Complete
+
+</button>
+
+</div>
+
+)
+
+}
